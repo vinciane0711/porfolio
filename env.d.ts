c78c5e93d64e4d176c59fd6a48e584c1ca0c3ca4
Qiqily0911 <angelccchen0911@gmail.com>
2024-08-19 23:40:30 +0800

    add GTAG script

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+/* eslint-disable */
+export declare global {
+  interface Window {
+    dataLayer?: any
+  }
+}
+/* eslint-enable */
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import './assets/main.scss'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+function addGtag() {
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${
+    import.meta.env.VITE_GTAG
+  }`
+  document.head.appendChild(script)
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || []
+    function gtag(str: string, param: any) {
+      window.dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+    gtag('config', import.meta.env.VITE_GTAG)
+  }
+}
+
+if (!import.meta.env.DEV) addGtag()
+
 const app = createApp(App)
-app.use(createPinia())
 app.use(router)
 app.mount('#app')
